f47e08dca7bea4b6c0361a0526842de09532df69
Duy <diidduong@gmail.com>
2024-02-24 12:46:56 -0700

    add sam test

---
+++ aws-crc/tests/unit/test_handler.py
@@ -0,0 +1,22 @@
+import json
+
+import pytest
+
+from hello_world import app
+
+
+@pytest.fixture()
+def apigw_event():
+    """ Generates API GW Event"""
+
+    return {}
+
+
+def test_lambda_handler(apigw_event):
+
+    ret = app.lambda_handler(apigw_event, "")
+    data = json.loads(ret["body"])
+
+    assert ret["statusCode"] == 200
+    assert "message" in ret["body"]
+    assert data["message"] == "hello yoo"
